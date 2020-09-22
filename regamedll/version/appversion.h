a59cb0505b8b0a3918cf0ebeea128919e6466315
xhsu <xujiananxm@gmail.com>
2020-09-22 21:40:22 +0800

    gitignore adjustment

---
+++ regamedll/version/appversion.h
@@ -0,0 +1,19 @@
+#ifndef __APPVERSION_H__
+#define __APPVERSION_H__
+
+// 
+// This file is generated automatically.
+// Don't edit it.
+// 
+
+// Version defines
+#define APP_VERSION "5.14.0.0-dev"
+
+#define APP_COMMIT_DATE "Feb 25 2020"
+#define APP_COMMIT_TIME "14:10:58"
+
+#define APP_COMMIT_SHA ""
+#define APP_COMMIT_URL ""
+
+#endif //__APPVERSION_H__
+
