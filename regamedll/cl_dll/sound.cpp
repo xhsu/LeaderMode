0183f12313607fa9e7587bca3c89daf00737ed42
xhsu <xujiananxm@gmail.com>
2020-10-15 19:41:24 +0800

    3D Sound Attempt #1

--- regamedll/cl_dll/sound.cpp
+++ regamedll/cl_dll/sound.cpp
@@ -10,17 +10,23 @@ Code - Luna the Reborn
 #include "precompiled.h"
 
 //#define FMOD_VECTOR Vector
-#include "../lib/FMOD/inc/fmod.hpp"
+#include "../external/FMOD/fmod.hpp"
 
 FMOD::System* gFModSystem = nullptr;
 
 static double g_flClientTime = 0;
 static double g_flClientTimeDelta = 0;
+static Vector g_vecPlayerLastPos = g_vecZero;
 
 static FMOD::Sound* sound1;
-static FMOD::Channel* channel3;
+static FMOD::Channel* channel1;
 
 static std::unordered_map<std::string, FMOD::Sound*> g_mapSoundPrecache;
+
+inline FMOD_VECTOR VecConverts(const Vector& v)
+{
+	return FMOD_VECTOR({ v.x * SND_DISTANCEFACTOR, v.z * SND_DISTANCEFACTOR, v.y * SND_DISTANCEFACTOR });
+}
 
 void Sound_Init()
 {
@@ -35,15 +41,22 @@ void Sound_Init()
 	*/
 	gFModSystem->set3DSettings(1.0, SND_DISTANCEFACTOR, 1.0f);
 
-	gFModSystem->createSound("drumloop.wav", FMOD_2D|FMOD_LOOP_OFF, 0, &sound1);
-	//sound1->set3DMinMaxDistance(0.5f * SND_DISTANCEFACTOR, 5000.0f * SND_DISTANCEFACTOR);
-	//sound1->setMode(FMOD_LOOP_NORMAL);
-	gFModSystem->playSound(sound1, 0, false, &channel3);
+	gFModSystem->createSound("drumloop.wav", FMOD_3D, 0, &sound1);
+	sound1->set3DMinMaxDistance(0.5f * SND_DISTANCEFACTOR, 300.0f * SND_DISTANCEFACTOR);
+	sound1->setMode(FMOD_LOOP_NORMAL);
+	//gFModSystem->playSound(sound1, 0, false, &channel1);
 }
 
 void TestFMOD()
 {
-	gFModSystem->playSound(sound1, 0, false, &channel3);
+	FMOD_VECTOR zero = { 0, 0, 0 };
+
+	bool paused;
+	channel1->getPaused(&paused);
+
+	gFModSystem->playSound(sound1, 0, true, &channel1);
+	channel1->set3DAttributes(&zero, &zero);
+	channel1->setPaused(!paused);
 }
 
 void PlaySound(const char* szSound)
@@ -56,7 +69,7 @@ void PlaySound(const char* szSound)
 		gFModSystem->createSound(strKey.c_str(), FMOD_2D | FMOD_LOOP_OFF, 0, &g_mapSoundPrecache[strKey]);
 	}
 
-	gFModSystem->playSound(g_mapSoundPrecache[strKey], 0, false, &channel3);
+	gFModSystem->playSound(g_mapSoundPrecache[strKey], 0, false, &channel1);
 }
 
 void Sound_Think(double flTime)
@@ -64,20 +77,19 @@ void Sound_Think(double flTime)
 	g_flClientTimeDelta = flTime - g_flClientTime;
 	g_flClientTime = flTime;
 
-	static FMOD_VECTOR lastpos = { 0.0f, 0.0f, 0.0f };
-	FMOD_VECTOR forward = { 0.0f, 0.0f, 1.0f };
-	FMOD_VECTOR up = { 0.0f, 1.0f, 0.0f };
-	FMOD_VECTOR vel;
+	Vector vecFwd, vecRight, vecUp;
+	gEngfuncs.pfnAngleVectors(gPseudoPlayer.pev->v_angle, vecFwd, vecRight, vecUp);
 
-	// vel = how far we moved last FRAME (m/f), then time compensate it to SECONDS (m/s).
-	//vel.x = (listenerpos.x - lastpos.x) * (1000 / INTERFACE_UPDATETIME);
-	//vel.y = (listenerpos.y - lastpos.y) * (1000 / INTERFACE_UPDATETIME);
-	//vel.z = (listenerpos.z - lastpos.z) * (1000 / INTERFACE_UPDATETIME);
+	// dv = ds / dt
+	Vector vecVel = (gPseudoPlayer.pev->origin - g_vecPlayerLastPos) / g_flClientTimeDelta;
+	g_vecPlayerLastPos = gPseudoPlayer.pev->origin;
 
-	// store pos for next time
-	//lastpos = listenerpos;
+	FMOD_VECTOR pos = VecConverts(gPseudoPlayer.pev->origin);
+	FMOD_VECTOR forward = VecConverts(vecFwd);
+	FMOD_VECTOR up = VecConverts(vecUp);
+	FMOD_VECTOR vel = VecConverts(vecVel);
 
-	//gFModSystem->set3DListenerAttributes(0, &g_vecZero, g_vecZero, &forward, &up);
+	gFModSystem->set3DListenerAttributes(0, &pos, &vel, &forward, &up);
 	gFModSystem->update();
 }
 
